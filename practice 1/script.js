855c9def88dda3d81feb0ab916ae962e97702e0a
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-05 14:45:07 +0200

    hoisting, functions and if

--- practice 1/script.js
+++ practice 1/script.js
@@ -23,8 +23,41 @@
 
 //DYNAMIC TYPING
 
-var var1 = 5;
-var Var1 = '10';
+// var var1 = 5;
+// var Var1 = '10';
 
-console.log(typeof var1);
-console.log(typeof Var1);
+// console.log(typeof var1);
+// console.log(typeof Var1);
+
+// HOISTING
+
+// var1 = 5;
+
+// console.log(var1);
+
+// var var1;
+
+// FUNCTIONS
+
+// function calc(number1, number2) {
+//     return number1+number2;
+// }
+
+// calc();
+// console.log(typeof calc);
+
+// var anotherFn = calc(1,2);
+// console.log(anotherFn);
+
+//IF STATEMENTs
+
+var condition = 1;
+var anotherCondition = null;
+
+if (condition) {
+    console.log('executed')
+} else if (anotherCondition) {
+    console.log('still executed')
+} else {
+    console.log('not executed')
+}
