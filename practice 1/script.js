40153e333720d4d64943fdd769a257da7f9250d3
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-05 14:09:25 +0200

    strict mode and dynamic

--- practice 1/script.js
+++ practice 1/script.js
@@ -3,12 +3,28 @@
 
 // VARIABLES - BASICS
 
- var variable = 'text';
- console.log(variable)
+//  var variable = 'text';
+//  console.log(variable)
 
- var var1 = [1,2,3,'a'];
- console.log(typeof var1);
- console.log(var1[0]);
- console.log(typeof NaN);
- console.log(typeof null);
- console.log(typeof undefined);
+//  var var1 = [1,2,3,'a'];
+//  console.log(typeof var1);
+//  console.log(var1[0]);
+//  console.log(typeof NaN);
+//  console.log(typeof null);
+//  console.log(typeof undefined);
+
+// OBJECTS
+// var var1 = {
+//     name:'Ivan'
+// };
+// console.log(var1.name);
+
+//STRICT MODE
+
+//DYNAMIC TYPING
+
+var var1 = 5;
+var Var1 = '10';
+
+console.log(typeof var1);
+console.log(typeof Var1);
