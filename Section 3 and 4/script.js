ed0aac89bdb18f8e2fb72b5fc67ed4c2fdc661bc
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-05 21:58:50 +0200

    types, scope and arrays

---
+++ Section 3 and 4/script.js
@@ -0,0 +1,89 @@
+// var number = 5;
+// console.log(number);
+
+// var anotherNumber = number;
+// console.log(anotherNumber);
+
+// number = 12;
+// console.log(number);
+// console.log(anotherNumber);
+
+// var array = [1,2,3];
+// console.log(typeof array);
+
+// var anotherArray = array;
+
+// console.log(array);
+// console.log(anotherArray);
+
+// array.push(4);
+
+// console.log(array);
+// console.log(anotherArray);
+
+// SCOPE
+
+// var test = 'Global scope';
+
+// function localScope() {
+//     var test = 'Local scope'
+//     console.log(test);
+// }
+
+// localScope();
+// console.log(test);
+
+// ARRAYS
+
+// var array = [1,2,3];
+// array[3] = 100;
+
+// console.log(array.length);
+// console.log(array);
+
+// array.forEach((e)=>{
+//     console.log(e)
+// })
+
+// array.push(4);
+// console.log(array);
+
+// console.log(array.pop());
+// console.log(array);
+
+// array.unshift('unshift');
+// console.log(array);
+
+var arr = [0,1,2,3,4];
+// arr.unshift('new');
+// console.log(arr.indexOf('new'))
+
+// arr[arr.indexOf(2)]='two';
+// console.log(arr);
+
+// var newArray = arr.splice(2,2)
+// console.log(newArray)
+// console.log(arr); 
+
+// console.log(arr.filter((e)=>e>2));
+
+// console.log(arr.map((e)=>e*2))
+
+// console.log(arr.reverse())
+
+// var arr2 = ['join','me'];
+// console.log(arr.concat(arr2))
+// console.log(arr.join(arr2))
+
+// console.log(arr2.join(' '));
+
+// console.log(arr.reduce((a,e)=>{
+//     console.log('total: ' + a + ', Value: ' + e)
+//     return a+e;
+// }))
+
+
+
+
+
+
