6a28156a365afd6a9941cc5995a2fd8ff0e81172
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-05 12:36:16 +0200

    javascript and variables

---
+++ practice 1/script.js
@@ -0,0 +1,14 @@
+// console.log('Hello world!');
+// console.log('Hello Plunker!');
+
+// VARIABLES - BASICS
+
+ var variable = 'text';
+ console.log(variable)
+
+ var var1 = [1,2,3,'a'];
+ console.log(typeof var1);
+ console.log(var1[0]);
+ console.log(typeof NaN);
+ console.log(typeof null);
+ console.log(typeof undefined);
