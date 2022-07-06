574441f3834f0f5b32ac6100da904214e6549cbf
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-06 13:47:35 +0200

    errors

--- Section 5 and 6/script.js
+++ Section 5 and 6/script.js
@@ -185,7 +185,14 @@
 //     console.log(person[field])
 // }
 
-
+// ERROS and DEBUGGING
+try{
+    test();
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log('Finally!')
+}
 
 
 
