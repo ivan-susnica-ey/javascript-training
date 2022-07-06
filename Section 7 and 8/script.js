ee6ef6eec9f1124e4dd1024e73c0052809234fca
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-06 14:23:37 +0200

    functions - advanced

---
+++ Section 7 and 8/script.js
@@ -0,0 +1,41 @@
+// CLOSURES
+//  function generator(input) {
+//     var number = input;
+//     return function() {
+//         return number*2
+//     }
+//  }
+
+//  var calc = generator(900);
+//  console.log(calc);
+//  console.log(calc())
+
+//  console.log(generator(900)())
+
+//IIFE
+
+// (function calc() {
+//     console.log('calc')
+// })()
+
+// (function calc(input) {
+//     var number = input;
+//     console.log(number)
+// })(10)
+
+// BUILT IN METHODS and PROPERTIES
+
+//  function message(message) {
+//     console.log(message);
+//     console.log(arguments[1 ]);
+//  }
+
+//  message('hi','hello')
+
+//  function message(book, house, dream) {
+//     console.log(book);
+//  }
+
+//  console.log(message.name)
+//  console.log(message.length)
+
